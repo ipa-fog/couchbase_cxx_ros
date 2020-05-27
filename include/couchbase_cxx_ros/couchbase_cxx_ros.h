59ebc0b08ebc59ca32de82e4cfb2b8499fee2f55
FOG <you@example.com>
2020-05-27 09:27:30 +0200

    added missing file

---
+++ include/couchbase_cxx_ros/couchbase_cxx_ros.h
@@ -0,0 +1,6 @@
+#include <libcouchbase/couchbase++.h>
+#include <libcouchbase/couchbase++/views.h>
+#include <libcouchbase/couchbase++/query.h>
+#include <libcouchbase/couchbase++/endure.h>
+#include <libcouchbase/couchbase++/logging.h>
+
