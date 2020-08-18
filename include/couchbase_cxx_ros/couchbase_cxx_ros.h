31bb835ffe5b8c6b3d7d92ddcd6ab704ae8c006d
Florenz Graf <florenz.graf@ipa.fraunhofer.de>
2020-08-18 14:05:03 +0200

    added include files

---
+++ include/couchbase_cxx_ros/couchbase_cxx_ros.h
@@ -0,0 +1,7 @@
+#include <libcouchbase/couchbase++.h>
+#include <libcouchbase/couchbase++/views.h>
+#include <libcouchbase/couchbase++/query.h>
+#include <libcouchbase/couchbase++/endure.h>
+#include <libcouchbase/couchbase++/logging.h>
+
+
